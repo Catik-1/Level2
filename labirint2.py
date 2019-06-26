745e59fd8b3110e76866d79437365194b41eb8e7
dragon26200387 <igor26200387@gmail.com>
2019-06-26 20:33:39 +0300

    Level2

---
+++ labirint2.py
@@ -0,0 +1,17 @@
+import turtle 
+
+wn = turtle.Screen()
+wn.bgpic('labirint2.gif')
+mouse = turtle.Turtle()
+mouse.up()
+mouse.color('red')
+mouse.width(2)
+mouse.goto(-350, 0)
+
+mouse.down()
+mouse.fd(40)
+
+wn.exitonclick()
+
+
+
